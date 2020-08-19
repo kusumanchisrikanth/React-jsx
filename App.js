190fc4c442ad0fb9847f54f903e711aaf3715bb5
kusumanchisrikanth <64022117+kusumanchisrikanth@users.noreply.github.com>
2020-08-19 13:44:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Movies from "./components/movies";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <Movies />
+    </div>
+  );
+}
+
+export default App;
