41836af7c45ff7e146705462b34f230b15f8a3b3
kusumanchisrikanth <64022117+kusumanchisrikanth@users.noreply.github.com>
2020-08-23 21:31:12 +0530

    Add files via upload

---
+++ pagination.js
@@ -0,0 +1,6 @@
+import _ from "lodash";
+
+export function paginate(items, pagenumber, pagesize) {
+  const startindex = (pagenumber - 1) * pagesize;
+  return _(items).slice(startindex).take(pagesize).value();
+}
